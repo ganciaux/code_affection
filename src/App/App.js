96229d7c6b85f8b85af2f106205d24646e5c205f
ghislain.anciaux <ghislain.anciaux@internet.lu>
2021-10-25 14:01:26 +0200

    Add SideMenu

---
+++ src/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import SideMenu from '../components/SideMenu';
+
+function App() {
+  return (
+    <>
+    <SideMenu></SideMenu>
+    <div>test</div>
+    </>
+  );
+}
+
+export default App;
