c59f4702c9ba8f1098b6eb0cc2723b0f075e46d7
ghislain.anciaux <ghislain.anciaux@internet.lu>
2021-10-25 16:53:17 +0200

    Change css to useStyles

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import './App.css';
-import SideMenu from '../components/SideMenu';
+import React from "react";
+import "./App.css";
+import SideMenu from "../components/SideMenu";
 
 function App() {
   return (
     <>
-    <SideMenu></SideMenu>
-    <div>test</div>
+      <SideMenu />
+      <div>test</div>
     </>
   );
 }
--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import { makeStyles, withStyles } from '@mui/styles';
+
+const useStyles = makeStyles({
+    SideMenu:{
+        display: 'flex',
+        flexDirection: 'column',
+        position: 'absolute',
+        left: '0px',
+        width: '320px',
+        height: '100vh',
+        backgroundColor:'#253053',
+    }
+});
 
 export default function SideMenu() {
+    const classes = useStyles();
     return (
-        <div className="side-menu">
+        <div className={classes.SideMenu}>
             
         </div>
     )
