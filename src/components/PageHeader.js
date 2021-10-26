fb7266f6bf116a8e08225b6bf4c6f2cbe5d39c76
ghislain.anciaux <ghislain.anciaux@internet.lu>
2021-10-26 16:30:34 +0200

    Add page header style

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -1,13 +1,37 @@
-import { Card, Paper } from '@mui/material'
+import { Card, Paper, Typography } from '@mui/material'
+import { makeStyles } from '@mui/styles'
 import React from 'react'
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: '#fdfdff',
+  },
+  pageHeader: {
+    padding: theme.spacing(4),
+    display: 'flex',
+    marginBottom: theme.spaging(2),
+  },
+  pageIcon:{
+    display: 'inline-block'
+  }
+}))
+
 export default function PageHeader(props) {
+  const classes = useStyles()
   const { title, subTitle, icon } = props
   return (
-    <Paper elevation={0} square>
-      <div>
-        <Card>{icon}</Card>
-      </div>
-    </Paper>
+      <Paper elevation={0} square className={classes.root}>
+        <div className={classes.pageHeader}>
+          <Card>{icon}</Card>
+          <div>
+            <Typography variant="h6" component="div">
+              {title}
+            </Typography>
+            <Typography variant="subtitle2" component="div">
+              {subTitle}
+            </Typography>
+          </div>
+        </div>
+      </Paper>
   )
 }
